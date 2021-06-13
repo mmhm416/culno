4a6d55777c1bf1b6d4cdaf1691510d92d1ca5bb2
mmhm416 <tma.hshmt@gmail.com>
2021-06-14 05:50:53 +0900

    バリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,21 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+    
+  has_one :address
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :level
-  has_one :address
+
+  with_options presence: true do
+    validates :nickname
+    validates :birthday
+    validates :level_id
+  end
+
+    with_options uniqueness: { case_sensitive: true } do
+      validates :nickname
+      validates :email
+    end
+    
 end
