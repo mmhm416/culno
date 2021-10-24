acc20e5cc14bca3936d8a83972146b625abfc4c8
mmhm416 <tma.hshmt@gmail.com>
2021-10-25 04:30:58 +0900

    バリデーション変更　＆　ユーザーモデルテスト

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,14 @@ class User < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :level
 
+  with_options numericality: { other_than: 0, message: 'Select' } do
+    validates :level_id
+  end
+
+  with_options format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'include both letters and numbers' } do
+    validates :password
+  end
+
   with_options presence: true do
     validates :nickname
     validates :birthday
